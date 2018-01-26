5057d5fdaaceb5915f772cd5726d8bcd78269c87
Himank Goel <himankgoel03@gmail.com>
2018-01-27 01:29:57 +0530

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Navbar} from 'bloomer';
+import { NavbarBrand } from 'bloomer/lib/components/Navbar/NavbarBrand';
+import { NavbarItem } from 'bloomer/lib/components/Navbar/NavbarItem';
+import { NavbarMenu } from 'bloomer/lib/components/Navbar/NavbarMenu';
+import { NavbarEnd } from 'bloomer/lib/components/Navbar/NavbarEnd';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navbar className = "navbar">
+          <NavbarBrand>
+            <NavbarItem>
+              <img src={logo} className = "navbar-logo" /> <span className = "logo-name">Trip-a-Treat</span>
+            </NavbarItem>
+            <NavbarItem className = "hidden-links">
+              <span className = "navbar-links">Login</span>
+            </NavbarItem>
+            <NavbarItem className = "hidden-links">
+              <span className = "navbar-links">SignUp</span>
+            </NavbarItem>
+          </NavbarBrand>
+          <NavbarMenu>
+            <NavbarEnd>
+              <NavbarItem>
+                <span className = "navbar-links">Login</span>
+              </NavbarItem>
+              <NavbarItem>
+                <span className = "navbar-links">SignUp</span>
+              </NavbarItem>
+            </NavbarEnd>
+          </NavbarMenu>
+        </Navbar>
       </div>
     );
   }
